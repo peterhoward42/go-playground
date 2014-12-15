103caf49e6705d9c95c789ad6f9eff3597751576
Peter Howard <peterhoward42@gmail.com>
2014-12-15 22:30:55 +0000

    Add templates to svr

--- play_svr/svr.go
+++ play_svr/svr.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -9,18 +10,28 @@ func fooHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "got foo request")
 }
 
-func barHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "got bar request")
+func templateHandler(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.New("myTemplate").Parse(HTML_TEMPLATE))
+	data := map[string]string{"subst": "injectedval"}
+	t.Execute(w, data)
 }
 
 func staticHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, r.URL.Path[1:])
 }
 
+const HTML_TEMPLATE = `
+    <html>
+        <body>
+            this is the template param with <b>{{.subst}}</b>
+        </body>
+    </html>
+`
+
 func main() {
 	fmt.Printf("hello world")
 	http.HandleFunc("/foo", fooHandler)
-	http.HandleFunc("/bar", barHandler)
+	http.HandleFunc("/template", templateHandler)
 	http.HandleFunc("/static/", staticHandler)
 	http.ListenAndServe(":9876", nil)
 }
