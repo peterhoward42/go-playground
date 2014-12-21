d1402c85bd7c37651a68cdaec2126b3baffb7e19
Peter Howard <peterhoward42@gmail.com>
2014-12-21 09:16:10 +0000

    Put in real HTML and Bootstrap CSS

--- play_svr/svr.go
+++ play_svr/svr.go
@@ -1,11 +1,19 @@
+/* This single-file program is a reference example of a minimal web server.
+   It includes examples of three types of handler: templated html, static file
+   serving and on-the-fly logic generated html. It also includes the minimum
+   Bootstrap CSS implementation, in the templated response.
+
+   It expects to find a directory called "templates" in the same place as the
+   server executable. It serves on a (hard coded) port number of 9876.
+*/
 package main
 
 import (
 	"fmt"
 	"html/template"
 	"net/http"
-    "os"
-    "path"
+	"os"
+	"path"
 )
 
 func fooHandler(w http.ResponseWriter, r *http.Request) {
@@ -25,9 +33,9 @@ var templateFile string
 var htmlTemplate *template.Template
 
 func main() {
-    curWd, _ := os.Getwd()
-    templateFile = path.Join(curWd, "templates", "foo.html")
-    htmlTemplate = template.Must(template.ParseFiles(templateFile))
+	curWd, _ := os.Getwd()
+	templateFile = path.Join(curWd, "templates", "foo.html")
+	htmlTemplate = template.Must(template.ParseFiles(templateFile))
 
 	http.HandleFunc("/foo", fooHandler)
 	http.HandleFunc("/template", templateHandler)
