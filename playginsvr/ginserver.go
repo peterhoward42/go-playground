57764ddb433e59aee97482fbbdd02549b34961dc
Peter Howard <pete.howard@dnae.com>
2018-09-15 22:49:35 +0100

    Added a few tests to illustrate some gin features.

--- playginsvr/ginserver.go
+++ playginsvr/ginserver.go
@@ -1,16 +1,48 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 // setupRouter is is a function that can be used to set up routes
 // in a reusable and deterministic way, good for incorporating into a
 // server, *and* for unit tests.
 func setupRouter() *gin.Engine {
 	r := gin.Default()
+
+	// The canonical route/handler.
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
+
+	// A route with a *compulsory* variable segment.
+	r.GET("/animal/:animalType", func(c *gin.Context) {
+		// Extract the variable value and return it as a string.
+		animalType := c.Param("animalType")
+		c.String(http.StatusOK, animalType)
+	})
+
+	// A route with both a *compulsory* and optional variable segments.
+	r.GET("/animal/:animalType/*action", func(c *gin.Context) {
+		// Extract the compulsory and optional variable value (if is present)
+		// and return them as a string.
+		animalType := c.Param("animalType")
+		action := c.Param("action")
+		c.String(http.StatusOK, "Animal type: <%s>, action: <%s>.",
+			animalType, action)
+	})
+
+	// How to extract query strings from GET URLs.
+	r.GET("/shouldhaveqryparams", func(c *gin.Context) {
+		price := c.Query("price")
+		weight := c.DefaultQuery("weight", "1Kg")
+		c.String(http.StatusOK, "Price: <%s>, Weight: <%s>", price, weight)
+	})
+
 	return r
+
 }
 
 // main is the function that brings up the server. It delegates the setting up
--- playginsvr/ginserver_test.go
+++ playginsvr/ginserver_test.go
@@ -26,3 +26,38 @@ func TestPingRoute(t *testing.T) {
 	assert.Equal(t, 200, w.Code)             // Response code OK
 	assert.Equal(t, "pong", w.Body.String()) // Body content.
 }
+
+// Show how the handlers can easily extract part of the URL (in this case
+// the *cat* part when the route is declared in terms of a compulsory variable.
+func TestUsingCompulsoryVariablesInRoute(t *testing.T) {
+	router := setupRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/animal/cat", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "cat", w.Body.String())
+}
+
+// Show how the handlers can fish out optional and compulsory URL segments.
+func TestUsingOptionalVariablesInRoute(t *testing.T) {
+	router := setupRouter()
+	w := httptest.NewRecorder()
+	// First furnish both bits.
+	req, _ := http.NewRequest("GET", "/animal/cat/stroke", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "Animal type: <cat>, action: </stroke>.", w.Body.String())
+}
+
+// A comment.
+func TestExtractingQueryParams(t *testing.T) {
+	router := setupRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/shouldhaveqryparams?price=£3.14&", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "Price: <£3.14>, Weight: <1Kg>", w.Body.String())
+}
