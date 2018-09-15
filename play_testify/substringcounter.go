cb04b62d233409267e114190717cd6177d644e32
Peter Howard <pete.howard@dnae.com>
2018-09-15 17:12:45 +0100

    Few tweaks arising from trying out Visual Studio Code and it's warnings.

--- play_testify/substringcounter.go
+++ play_testify/substringcounter.go
@@ -1,29 +1,26 @@
-// Package play_testify contains code that explores the use of mocking
+// Package playtestify contains code that explores the use of mocking
 // services in the "github.com/stretchr/testify/mock" package.
-package play_testify
+package playtestify
 
 import (
-    "strings"
+	"strings"
 )
 
-
-
-//The function CountSubstrings is the code that we are going to write
+//CountSubstrings is the function that we are going to write
 // a test for. It depends on a message getter object passed in - which we
 // are going to provide in the test using a testify mock object.
 func CountSubstrings(getter MessageGetter) int {
-    // Returns how many of the first hundred messages retreived from the
-    // message getter, contain the substring "01".
-    count :=0
-    for i := 0; i < 100; i++ {
-        message := getter.NextMessage()
-        if strings.Contains(message, "01") {
-            count += 1
-        }
-    }
-    return count
+	// Returns how many of the first hundred messages retreived from the
+	// message getter, contain the substring "01".
+	count := 0
+	for i := 0; i < 100; i++ {
+		message := getter.NextMessage()
+		if strings.Contains(message, "01") {
+			count++
+		}
+	}
+	return count
 }
-
 
 // The MessageGetter interface offers to fetch messages from its source
 // one at a time.
--- play_testify/substringcounter_test.go
+++ play_testify/substringcounter_test.go
@@ -1,52 +1,49 @@
-package play_testify
+package playtestify
 
 import (
 	"testing"
 
-    "github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/mock"
 )
-
 
 // The TestCountsCorrectly function is a unit test that illustrates the use of
 // mock.Mock.
 func TestCountsCorrectly(t *testing.T) {
 
-    // Prepare a mock object that satisfies the MessageGetter interface.
-    getter := MockMessageGetter{}
+	// Prepare a mock object that satisfies the MessageGetter interface.
+	getter := MockMessageGetter{}
 
-    // Notify it that it should expect its NextMessage method to be called
-    // at least once, with no arguments. And that when so, it should return the
-    // fixed string "fibble". This should make the calling-record assertions pass,
-    // but the test per-se to fail, because the test
-    // expects some of the returned messages among the first 100 read, to contain
-    // the substring "01".
-    getter.On("NextMessage").Return("fibble")
+	// Notify it that it should expect its NextMessage method to be called
+	// at least once, with no arguments. And that when so, it should return the
+	// fixed string "fibble". This should make the calling-record assertions pass,
+	// but the test per-se to fail, because the test
+	// expects some of the returned messages among the first 100 read, to contain
+	// the substring "01".
+	getter.On("NextMessage").Return("fibble")
 
-    // Call the function - passing in the mock message getter.
-    count := CountSubstrings(&getter)
+	// Call the function - passing in the mock message getter.
+	count := CountSubstrings(&getter)
 
-    // Make sure it got called as it was instructed to expect.
-    getter.AssertExpectations(t)
+	// Make sure it got called as it was instructed to expect.
+	getter.AssertExpectations(t)
 
-    // Finish up with a conventional test of the object under test - which should
-    // fail because the mock message getter doesn't deliver any messages with
-    // "01" in them.
+	// Finish up with a conventional test of the object under test - which should
+	// fail because the mock message getter doesn't deliver any messages with
+	// "01" in them.
 	if count != 43 {
 		t.Errorf("Expected 43, got <%d>", count)
 	}
 }
 
-
 // The MockMessageGetter struct is our mock object - a trivial wrapper round a
 // mock.Mock.
-type MockMessageGetter struct{
-  mock.Mock
+type MockMessageGetter struct {
+	mock.Mock
 }
 
-
-// The NextMessage method is the method for MockMessageGetter, that makes it 
+// The NextMessage method is the method for MockMessageGetter, that makes it
 // satisfy the MessageGetter interface.
-func (m *MockMessageGetter) NextMessage() (string) {
-    args := m.Called()
-    return args.String(0)
+func (m *MockMessageGetter) NextMessage() string {
+	args := m.Called()
+	return args.String(0)
 }
